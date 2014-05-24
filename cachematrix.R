fc413a4a7fa0f80f2ba72fcd43e74f65e644886a
nielsbach <nielsbach@hotmail.com>
2014-05-24 17:30:25 -0400

    Comments
    Added more comments to source

--- cachematrix.R
+++ cachematrix.R
@@ -1,7 +1,9 @@
 ## Matrix inversion is usually a costly computation and their may be some benefit to caching the inverse of a matrix rather than compute it repeatedly
 
-## This function creates a special "matrix" object that can cache its inverse.
+
 makeCacheMatrix <- function(x = matrix()) {
+  ## This function creates a special "matrix" object that can cache its inverse.
+  ## The function is called with a regular r matrix as a parameter.
   s <- NULL
   set <- function(y) {
     x <<- y
@@ -12,15 +14,27 @@ makeCacheMatrix <- function(x = matrix()) {
   getsolve <- function() s
   ## makeCacheMatrix returns a list of functions:
   
+  ## set(): set the value of the matrix
+  ## get(): get the value of the matrix
+  ## setsolve(): set the value of inverse of the matrix
+  ## getsolve(): get the value of inverse of the matrix
+  
   ## set/get of matrix, set/get of solution
   list(set = set, get = get,
        setsolve = setsolve,
        getsolve = getsolve)
 }
 
-## This function computes the inverse of the special "matrix" returned by makeCacheMatrix above. If the inverse has already been calculated (and the matrix has not changed), then the cachesolve should retrieve the inverse from the cache.
+## This function computes the inverse of the special "matrix" returned by makeCacheMatrix above. 
 cacheSolve <- function(x, ...) {
-  ## Return a matrix that is the inverse of 'x'
+  ## Return a matrix that is the inverse of 'x', where x is the object returned by makeCacheMatrix()
+  ## 
+  ## This function assumes that the matrix is invertible
+  ##
+  ## First it checks if the inverse matrix has been computed already.
+  ## If so, it retrieves the result from the previous computation, otherwise if computes it
+  ## and stores the result in the cache for later use.
+  ##
   s <- x$getsolve()
   if(!is.null(s)) {
     message("getting cached data")
